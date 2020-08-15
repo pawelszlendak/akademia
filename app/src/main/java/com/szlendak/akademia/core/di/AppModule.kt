bd9e39c26a5a5c041d47bc79670984ace5514e09
pawelszlendak <pawel.szlendak@embiq.com>
2020-08-15 18:45:15 +0200

    dependency injection - AppModule, DatabaseModule, NetworkModule

---
+++ app/src/main/java/com/szlendak/akademia/core/di/AppModule.kt
@@ -0,0 +1,18 @@
+package com.szlendak.akademia.core.di
+
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+const val SPAN_COUNT = 2
+val appModule = module {
+
+    factory { LinearLayoutManager(androidContext()) }
+
+    factory { GridLayoutManager(androidContext(), SPAN_COUNT) }
+
+    factory { DividerItemDecoration(androidContext(), LinearLayoutManager.VERTICAL) }
+
+}
---
+++ app/src/main/java/com/szlendak/akademia/features/data/RickAndMortyApi.kt
@@ -0,0 +1,5 @@
+package com.szlendak.akademia.features.data
+
+interface RickAndMortyApi {
+
+}
